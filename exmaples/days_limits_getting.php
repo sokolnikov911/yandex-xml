fe1d544a93e9e9b6f5b1f7b119d9fd27913ab64d
Petro Sokolnykov <info@xyz.net.ua>
2020-10-27 10:15:36 +0300

    add example

--- exmaples/days_limits_getting.php
+++ exmaples/days_limits_getting.php
@@ -4,8 +4,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use sokolnikov911\YandexXml\Client;
 
-$yandexXmlUser = 'xmlkat';
-$yandexXmlKey = '03.288092510:f4b921cca1e845e8299ef753838f934d';
+$yandexXmlUser = 'xmluser';
+$yandexXmlKey = '12.12344:123443543534';
 
 $client = new Client($yandexXmlUser, $yandexXmlKey);
 
